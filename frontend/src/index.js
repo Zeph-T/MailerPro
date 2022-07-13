e543c63974edbea4c088a018cbf1103b3a4466f7
Raj <varasaniraj5@gmail.com>
2022-07-13 17:33:01 +0530

    Setup Frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+
+import { BrowserRouter as Router } from "react-router-dom";
+
+
+import { Provider } from "react-redux";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+import { ConfigureStore } from "./Redux/ConfigureStore";
+
+const store = ConfigureStore();
+
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    
+      <Provider store={store}>
+    
+      
+      <Router>
+      
+        <App />
+      
+      </Router>
+      
+    
+      </Provider>
+    
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
