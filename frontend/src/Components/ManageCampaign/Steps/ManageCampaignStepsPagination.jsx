7aa9ed3c2489f0db04c6cc3ae7dbdb4d9bb5af27
Raj <varasaniraj5@gmail.com>
2022-07-17 20:39:20 +0530

    Created general ManageCampaignStepsWrapper comp

---
+++ frontend/src/Components/ManageCampaign/Steps/ManageCampaignStepsPagination.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import StyledMUIButton from "../../General/Helpers/StyledMUIButton";
+import styles from "./ManageCampaignStepsPagination.module.css";
+import { MANAGE_CAMPAIGN_DATA } from "./../../../Utils/staticData";
+
+function ManageCampaignStepsPagination({
+  currentState,
+  totalStates,
+  handleNext,
+  handleBack,
+}) {
+  return (
+    <div className={styles.Wrapper}>
+      {currentState !== 0 && (
+        <StyledMUIButton
+          style={{ maxWidth: "17rem" }}
+          fullWidth
+          color="buttonGrey"
+          onClick={handleBack}
+        >
+          {MANAGE_CAMPAIGN_DATA.back}
+        </StyledMUIButton>
+      )}
+      {currentState < totalStates - 1 && (
+        <StyledMUIButton
+          style={{ maxWidth: "17rem" }}
+          fullWidth
+          color="buttonGreen"
+          onClick={handleNext}
+        >
+          {MANAGE_CAMPAIGN_DATA.next}
+        </StyledMUIButton>
+      )}
+    </div>
+  );
+}
+
+export default ManageCampaignStepsPagination;
