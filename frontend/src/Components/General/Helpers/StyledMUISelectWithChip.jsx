af85ebf2614faa4f20a3b02c91583daf7126635e
Raj <varasaniraj5@gmail.com>
2022-08-01 14:39:07 +0530

    Removed unused props in StyledMUISelectWithChip

--- frontend/src/Components/General/Helpers/StyledMUISelectWithChip.jsx
+++ frontend/src/Components/General/Helpers/StyledMUISelectWithChip.jsx
@@ -1,49 +1,15 @@
 import React from "react";
 import { Autocomplete, TextField } from "@mui/material";
 
-const StyledMUISelectWithChip = ({
-  label,
-  placeholder,
-  chipClass,
-  handleDelete,
-  ...props
-}) => {
+const StyledMUISelectWithChip = ({ label, placeholder, ...props }) => {
   return (
     <Autocomplete
       multiple
       filterSelectedOptions
-      sx={{
-        fontSize: "var(--font-16) !important",
-      }}
       {...props}
       renderInput={(params) => (
-        <TextField
-          {...params}
-          InputProps={{
-            ...params.InputProps,
-            style: {
-              ...params.InputProps.style,
-              fontSize: 16,
-              borderRadius: 8,
-            },
-          }}
-          InputLabelProps={{
-            ...params.InputLabelProps,
-            style: {
-              ...params.InputLabelProps.style,
-              fontSize: 16,
-            },
-          }}
-          label={label}
-          placeholder={placeholder}
-        />
+        <TextField {...params} label={label} placeholder={placeholder} />
       )}
-      ChipProps={{
-        sx: { fontSize: 12 },
-      }}
-      ListboxProps={{
-        sx: { fontSize: 14 },
-      }}
     />
   );
 };
