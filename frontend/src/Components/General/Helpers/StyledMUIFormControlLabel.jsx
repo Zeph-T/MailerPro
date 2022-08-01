873f79294a41bd96350cb692976394e419301fda
Raj <varasaniraj5@gmail.com>
2022-08-01 14:35:18 +0530

    Updated props in StyledMUIFormControlLabel

--- frontend/src/Components/General/Helpers/StyledMUIFormControlLabel.jsx
+++ frontend/src/Components/General/Helpers/StyledMUIFormControlLabel.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { FormControlLabel, Typography } from "@mui/material";
 
-const StyledMUIFormControlLabel = ({ label, sx, ...props }) => {
+const StyledMUIFormControlLabel = ({ label, labelProps, ...props }) => {
   return (
     <FormControlLabel
-      label={<Typography sx={{ ...sx }}>{label}</Typography>}
+      label={<Typography {...labelProps}>{label}</Typography>}
       {...props}
     />
   );
