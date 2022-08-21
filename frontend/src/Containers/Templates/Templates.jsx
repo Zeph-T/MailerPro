8508b17deb63636bb4245fc597f4336d7a84687e
Raj <varasaniraj5@gmail.com>
2022-08-21 11:52:48 +0530

    Created MUI grid

--- frontend/src/Containers/Templates/Templates.jsx
+++ frontend/src/Containers/Templates/Templates.jsx
@@ -5,11 +5,48 @@ import Header from "./../../Components/Header/index";
 import { TEMPLATES_PAGE_DATA } from "./../../Utils/staticData";
 import CustomTabs from "../../Components/General";
 import StyledMUIButton from "./../../Components/General/Helpers/StyledMUIButton";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableFooter,
+  TablePagination,
+  TableRow,
+} from "@mui/material";
+
+const TEMP_TABLE_CONTACTS_DATA = {
+  email: new Array(50).fill({}).map((item, index) => {
+    return {
+      _id: index,
+      name: "Email Template " + index,
+    };
+  }),
+  sms: new Array(50).fill({}).map((item, index) => {
+    return {
+      _id: index,
+      name: "SMS Template " + index,
+    };
+  }),
+};
 
 function Templates() {
   const [currentTab, setCurrentTab] = React.useState(
     TEMPLATES_PAGE_DATA.tabs[0].value
   );
+  const [currentData, setCurrentData] = React.useState(
+    TEMP_TABLE_CONTACTS_DATA.email.slice(0, 10)
+  );
+  const [currentPage, setCurrentPage] = React.useState(0);
+
+  React.useEffect(() => {
+    setCurrentData(
+      TEMP_TABLE_CONTACTS_DATA[currentTab].slice(
+        currentPage * 10,
+        (currentPage + 1) * 10
+      )
+    );
+  }, [currentPage, currentTab]);
 
   return (
     <div className={styles.container}>
@@ -42,8 +79,48 @@ function Templates() {
           currentTab={currentTab}
           handleClick={(val) => {
             setCurrentTab(val);
+            setCurrentPage(0);
           }}
         />
+      </div>
+      <div className={styles.ContentWrapper}>
+        <TableContainer>
+          <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+            <TableBody>
+              {currentData.map((row) => (
+                <TableRow key={row.name}>
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell style={{ width: 160 }} align="right">
+                    {"10"}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+            <TableFooter>
+              <TablePagination
+                colSpan={3}
+                count={50}
+                rowsPerPage={10}
+                page={currentPage}
+                SelectProps={{
+                  inputProps: {
+                    "aria-label": "rows per page",
+                  },
+                  native: true,
+                }}
+                rowsPerPageOptions={[]}
+                onPageChange={(e, newPage) => {
+                  setCurrentPage(newPage);
+                }}
+                sx={{
+                  borderBottom: "none",
+                }}
+              />
+            </TableFooter>
+          </Table>
+        </TableContainer>
       </div>
     </div>
   );
