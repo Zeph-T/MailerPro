58a2d5ac0e15ccbc8b8b9bdb4c315a1473c5e32d
Akash Gupta <akash.gupta.anup@gmail.com>
2022-08-23 16:24:19 +0530

    Added minor stylings

--- frontend/src/Components/Chart/Chart.jsx
+++ frontend/src/Components/Chart/Chart.jsx
@@ -1,33 +1,68 @@
-import React from 'react'
-import 'chart.js/auto';
-import { Chart } from 'react-chartjs-2';
+import React from "react";
+import styles from "./Chart.module.css";
+import { Chart } from "react-chartjs-2";
+import "chart.js/auto";
 
-
-export default function LineChart({chartData}) {
+export default function LineChart({ chartData }) {
   return (
-    <div>
+    <div className={styles.Wrapper}>
       <Chart
-        type='line'
+        type="line"
+        style={{ width: "100%", height: "100%" }}
         data={chartData}
         options={{
           responsive: true,
+          scales: {
+            x: {
+              ticks: {
+                font: {
+                  size: 12,
+                  family: '"Poppins", sans-serif',
+                },
+              },
+            },
+            y: {
+              ticks: {
+                font: {
+                  size: 12,
+                  family: '"Poppins", sans-serif',
+                },
+              },
+            },
+          },
           plugins: {
             title: {
               display: true,
-              text: "Subscriber data for the past week"
+              text: "Subscriber data for the past week",
+              font: {
+                family: '"Poppins", sans-serif',
+                size: 20,
+              },
             },
             layout: {
-              padding: -2
+              padding: -2,
             },
             legend: {
               display: true,
-              position: 'top',
-           }
+              position: "top",
+              labels: {
+                font: {
+                  family: '"Poppins", sans-serif',
+                },
+              },
+            },
+            tooltip: {
+              bodyFont: {
+                family: '"Poppins", sans-serif', // Add your font here to change the font of your tooltip body
+              },
+              titleFont: {
+                family: '"Poppins", sans-serif', // Add your font here to change the font of your tooltip title
+              },
+            },
           },
           // clip: {left: 5, top: false, right: -2, bottom: 0}
         }}
       />
     </div>
   );
-};
-
+}
