1a18130514e79a6cda2580dbebd0c36000d84ea3
Raj <varasaniraj5@gmail.com>
2022-08-24 10:41:08 +0530

    Added null value in email

--- backend/server/models/contact.js
+++ backend/server/models/contact.js
@@ -1,49 +1,56 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const ContactSchema = new mongoose.Schema({
-    fname : {
-        type : String
+const ContactSchema = new mongoose.Schema(
+  {
+    fname: {
+      type: String,
     },
-    mname : {
-        type : String
+    mname: {
+      type: String,
     },
-    lname : {
-        type : String
+    lname: {
+      type: String,
     },
-    phone : {
-        type : String
+    phone: {
+      type: String,
+      unique: true,
+      sparse: true,
     },
-    email  : {
-        type : String
+    email: {
+      type: String,
+      unique: true,
+      sparse: true,
     },
-    dob : {
-        type : String,
-        default : ""
+    dob: {
+      type: String,
+      default: "",
     },
-    isValid : {
-        type : Boolean,
-        default : true
+    isValid: {
+      type: Boolean,
+      default: true,
     },
-    status : {
-        type : String,
-        default : "Subscribed" ,
-        enum   : ["Subscribed" , "Unsubscribed"]
+    status: {
+      type: String,
+      default: "Subscribed",
+      enum: ["Subscribed", "Unsubscribed"],
     },
-    createdOn : {
-        type : Date,
-        default : Date.now
+    createdOn: {
+      type: Date,
+      default: Date.now,
     },
-    updatedOn : {
-        type : Date,
-        default : Date.now
+    updatedOn: {
+      type: Date,
+      default: Date.now,
     },
-    createdBy : {
-        type : mongoose.Types.ObjectId,
-        ref : 'User'
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
     },
-    tags : []
-},{
-    strict : false
-});
+    tags: [],
+  },
+  {
+    strict: false,
+  }
+);
 
-export default mongoose.model('Contact', ContactSchema);
+export default mongoose.model("Contact", ContactSchema);
