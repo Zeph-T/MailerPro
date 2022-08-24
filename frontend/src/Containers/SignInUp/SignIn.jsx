54ace473cbc07f212165fc127c0bb1462ea6c9f3
Raj <varasaniraj5@gmail.com>
2022-08-24 06:38:53 +0530

    Integrated signin

--- frontend/src/Containers/SignInUp/SignIn.jsx
+++ frontend/src/Containers/SignInUp/SignIn.jsx
@@ -1,20 +1,40 @@
 import React from "react";
 import StyledMUIInput from "./Helpers/StyledMUIInput";
 import { useLocation } from "react-router-dom";
-import Button from "../../Components/Button";
+import { useCookies } from "react-cookie";
 import BottomText from "./Helpers/BottomText";
 import { SIGN_IN_DATA as signInData } from "../../Utils/staticData";
 import Styles from "./SignInUp.module.css";
+import { StyledMUIButton } from "../../Components/General/Helpers";
+import { login } from "../../Services/user.service";
 
 function SignIn() {
+  const [, setCookie] = useCookies();
   const location = useLocation();
-
   const formRef = React.useRef(123);
   const [isDisabled, setIsDisabled] = React.useState(false);
 
   const signIn = async (e) => {
     e.preventDefault();
-    setIsDisabled(false);
+    setIsDisabled(true);
+
+    const userData = {
+      email: formRef.current.elements.SignInEmail.value,
+      password: formRef.current.elements.SignInPassword.value,
+    };
+
+    try {
+      const response = await login(userData);
+      console.log(response);
+      setCookie("token", response.data.token, {
+        sameSite: "strict",
+        path: "/",
+      });
+    } catch (e) {
+      console.log(e);
+      setIsDisabled(false);
+      notify("Internal Server Error", "error");
+    }
   };
 
   return (
@@ -50,12 +70,18 @@ function SignIn() {
             autoComplete="current-password"
             disabled={isDisabled}
           />
-          <Button
-            content="Continue"
-            mainColor="#0079F1"
-            fontSize="var(--font-16)"
-            wrapperClass={Styles.SignInUpButton}
-          />
+          <StyledMUIButton
+            fullWidth
+            variant="contained"
+            style={{
+              boxShadow: "none",
+              marginTop: "2rem",
+            }}
+            type="submit"
+            disabled={isDisabled}
+          >
+            Continue
+          </StyledMUIButton>
         </form>
       </div>
       <div className={Styles.BottomSecWrapper}>
