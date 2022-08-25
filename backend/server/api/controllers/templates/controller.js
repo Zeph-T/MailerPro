a83cc1f4803ebc65b0031184df66eaa3853769f0
Zeph-T <paul.zeph@gmail.com>
2022-08-25 23:53:19 +0530

    adds sms campaigns anf template fix

--- backend/server/api/controllers/templates/controller.js
+++ backend/server/api/controllers/templates/controller.js
@@ -30,6 +30,7 @@ export class Controller {
 
   createTemplate(req, res) {
     isAuthenticated(req, res, () => {
+      req.body.templateType = req.params.type;
       Template.create(req.body).then(
         (r) => res.json({ data: r }),
         (error) => res.status(404).json({ data: { error: error } })
---
+++ backend/server/models/smsCampaign.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const CampaignSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : [true , "Name missing!"]
+    },
+    note : {
+        type : String
+    },
+    template : {
+        type : mongoose.Types.ObjectId,
+        ref : 'Template'
+    },
+    status : {
+        type : String,
+        enum : ["Draft" , "Running" , "Scheduled" , "Aborted"],
+        required: [true , "Status of Campaign Missing"]
+    },
+    isMarkedForImmediateSend : {
+        type: Boolean,
+        default : false,
+        required : true
+    },
+    scheduledTime : {
+        type : Date
+    },
+    targetAudience : {
+        audienceType : {
+            type : String,
+            enum : ["ALL" , "TAGS"]
+        },
+        tags : []
+    }
+})
+
+
+module.exports =  mongoose.model("smsCampaign" , CampaignSchema)
