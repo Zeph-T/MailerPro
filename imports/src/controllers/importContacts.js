d5cc4c372463f8bf7fd6ed726fed7bb1c347a60d
Nithin-Reddy-02 <jonnynithin7@gmail.com>
2022-08-26 02:34:21 +0530

    added batch processing for importing contacts

--- imports/src/controllers/importContacts.js
+++ imports/src/controllers/importContacts.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
+const util = require("node:util");
+const exec = util.promisify(require("node:child_process").exec);
 const http = require("http");
 const csv = require("csv-parser");
 const { parse } = require("json2csv");
-let exec = require("child_process").exec;
 const { array } = require("./multerStorage");
 const contact = require("../models/contact");
 const constants = require("../../constants");
@@ -21,7 +22,7 @@ module.exports = (req, res) => {
     }
     let randomString = makeid(5);
     req.body.tags = JSON.parse(req.body.tags);
-    let tags = req.body.tags.map(oTag=>oTag._id);
+    let tags = req.body.tags.map((oTag) => oTag._id);
     let Path = __basedir + "/uploads/" + req.file.filename;
     fs.createReadStream(Path)
       .pipe(csv())
@@ -32,35 +33,44 @@ module.exports = (req, res) => {
         arr = findRemainingHeaders(temp_headers);
         dataArray.push(data);
       })
-      .on("end", function () {
-        var result = parse(dataArray, {
-          fields: Object.keys(dataArray[0]),
-          header: false,
-        });
-        fs.writeFileSync("new.csv", result);
-        let newPath = __basedir + "/new.csv";
-        let command =
-          "mongoimport --uri " +
-          env.DB_STRING +
-          " -c contacts --type=csv --fields=" +
-          temp_headers.toString() +
-          " --file " +
-          `"${newPath}"` +
-          " --ignoreBlanks";
+      .on("end", async function () {
+        try {
+          for (let i = 0; i <= (dataArray.length - 1) / 50000; i++) {
+            var result = parse(
+              dataArray.slice(
+                i * 50000,
+                (i + 1) * 50000 - 1 < dataArray.length
+                  ? (i + 1) * 50000
+                  : dataArray.length - 1
+              ),
+              {
+                fields: Object.keys(dataArray[0]),
+                header: false,
+              }
+            );
+            fs.writeFileSync("new.csv", result);
+            let newPath = __basedir + "/new.csv";
+            let command =
+              "mongoimport --uri " +
+              env.DB_STRING +
+              " -c contacts --type=csv --fields=" +
+              temp_headers.toString() +
+              " --file " +
+              `"${newPath}"` +
+              " --ignoreBlanks";
 
-        exec(command, (err, stdout, stderr) => {
-          deleteCsv(Path);
-          if (err) {
-            console.log(err.message);
-          } else {
-            console.log("Success");
-
-            //API call to add tags to Backend
+            await exec(command);
             addTags(randomString, tags);
-
-            res.send("Successfully uploaded the CSV into Database");
+            console.log(`Completed Batch ${i}`);
           }
-        });
+          deleteCsv(Path);
+          res.send("Successfully uploaded the CSV into Database");
+        } catch (error) {
+          console.log(error);
+          res.status(500).send({
+            message: "Couldnot send request",
+          });
+        }
       });
   } catch (error) {
     console.log(error);
@@ -79,7 +89,6 @@ function findRemainingHeaders(headers) {
   }
   return arr;
 }
-
 
 function deleteCsv(Path) {
   fs.unlink(Path, (err) => {
@@ -102,18 +111,27 @@ function makeid(length) {
   return result;
 }
 
-
-
-async function addTags(hash,tags){
+async function addTags(hash, tags) {
   try {
-      contact.updateMany({hash:hash},{$set : {tags:tags, isValid:true , createdOn : new Date() , updatedOn : new Date() }}, {$unset : { hash : 1} },(err,doc)=>{
-          if(err){
-            console.log(err);
-          }
-          console.log({message:"Successfully added tags"})
-      })
+    contact.updateMany(
+      { hash: hash },
+      {
+        $set: {
+          tags: tags,
+          isValid: true,
+          createdOn: new Date(),
+          updatedOn: new Date(),
+        },
+      },
+      { $unset: { hash: 1 } },
+      (err, doc) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log({ message: "Successfully added tags" });
+      }
+    );
   } catch (error) {
     console.log(error);
   }
-  
 }
